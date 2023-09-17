3ba5bf4202d483fcbfc05f858224cab86db7a15e
claumigue <51464818+claumigue@users.noreply.github.com>
2023-09-17 04:43:35 -0300

    minor changes to the vittalia_delete_horarios_postmeta function

--- inc/vittalia-helper.php
+++ inc/vittalia-helper.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * Función para obtener los posts de tipo medilink_doctor que pertenecen a un término de la taxonomía medilink_doctor_category
+ * Obtener los posts de tipo medilink_doctor que pertenecen a un término de la taxonomía medilink_doctor_category
+ *
  * @param int $term El ID del término de la taxonomía
  * @return array El array de posts que cumplen con la condición
  */
@@ -21,4 +22,31 @@ function vittalia_get_department_doctors( $term ) {
 	// $_ids = wp_list_pluck($_posts, 'ID'); // obtienes el array de ids
 	wp_reset_postdata();
 	return $_posts;
+}
+
+/**
+ * Borrar los metadatos de tipo horario de determinado post
+ *
+ * @param int $post_id El ID del post
+ */
+function vittalia_delete_horarios_postmeta( $post_id ) {
+
+	if ( get_post_type( $post_id ) == 'medilink_doctor' ) {
+
+		// Definir el array con los nombres de los metadatos
+		$meta_keys = array(
+			'medilink_doctor_horarios_mon',
+			'medilink_doctor_horarios_tue',
+			'medilink_doctor_horarios_wed',
+			'medilink_doctor_horarios_thu',
+			'medilink_doctor_horarios_fri',
+			'medilink_doctor_horarios_sat',
+			'medilink_doctor_horarios_sun',
+		);
+
+		// Recorrer el array y borrar cada metadato
+		foreach ( $meta_keys as $meta_key ) {
+			delete_post_meta( $post_id, $meta_key );
+		}
+	}
 }
